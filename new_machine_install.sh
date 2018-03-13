7e4d66cbe67d93dc78cb53ade9f094146402246d
NPCHAMPION <axis139@gmail.com>
2018-03-13 11:25:51 -0700

    added backup script and changes

--- new_machine_install.sh
+++ new_machine_install.sh
@@ -2,3 +2,27 @@ cd ~
 git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts.git && ./install.sh
 
+cd ~
+git clone https://github.com/mct-dev/dotfiles.git
+
+echo "Need to download: \n\
+	\n  Chrome \
+	\n  Node.js (n node manager) \
+	\n  vscode - extensions: \
+	\n    quokka, advanced new file, auto-close tag, \
+	\n    auto rename tag, bracket pair colorizer, \
+	\n    eslint, gitignore, path intellisense, \
+	\n    todo highlight, vim, prettier/beautify \
+	\n  install vim stuff \
+	\n  albert? \
+	\n  docker and docker-compose \
+	\n  terminal color schemes \
+	\n  ruby? \
+	\n  oh-my-zsh \
+	\n  htop \
+	\n  variety \
+	\n  virtualbox \
+	\n  libreoffice \
+	\n  postman \
+
+	"
